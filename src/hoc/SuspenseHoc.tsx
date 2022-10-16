bd457d6a1b194680aea0e823588e7894859bcc90
Konstantin Dudkin <ttes7a@gmail.com>
2022-10-16 18:20:12 +0300

    refactor SuspenseHoc

--- src/hoc/SuspenseHoc.tsx
+++ src/hoc/SuspenseHoc.tsx
@@ -1,6 +1,6 @@
 import { ComponentType, Suspense } from 'react';
 
-export const SuspenseHoc = <T extends unknown>(Component: ComponentType) => {
+export const SuspenseHoc = (Component: ComponentType) => {
     return ( props: any ) => {
       return <Suspense fallback={ <div>Loading...</div> }>
             <Component {...props}/>
